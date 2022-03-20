287fc781bc3d068da955d47c8577e279a896e74e
Alexander Glering <algle11@student.sdu.dk>
2022-03-20 13:56:06 +0100

    TestSuite.class

--- SemProjFX/src/test/java/test/TestSuite.java
+++ SemProjFX/src/test/java/test/TestSuite.java
@@ -4,7 +4,8 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages({"src/test"})
+//@SelectPackages({"src/test"})
+@SelectClasses({ProductTest.class, ProductAttributeTest.class, JSONReaderTest.class, ProductManagerTest.class})
 public class TestSuite {
 
 }
