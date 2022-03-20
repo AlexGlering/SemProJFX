2917a946b967856a1e8633d537021943d5723d60
Alexander Glering <algle11@student.sdu.dk>
2022-03-20 13:52:24 +0100

    Updated dependencies + TestSuite

--- SemProjFX/src/test/java/test/TestSuite.java
+++ SemProjFX/src/test/java/test/TestSuite.java
@@ -4,8 +4,7 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ProductManagerTest.class, ProductAttributeTest.class, JSONReaderTest.class, ProductTest.class})
-//@SelectPackages({"test"})
+@SelectPackages({"src/test"})
 public class TestSuite {
 
 }
